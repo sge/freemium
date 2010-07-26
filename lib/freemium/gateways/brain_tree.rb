af009cc4f909bdea721ff546a3b7d4cab6cc7548
Eric Lindvall <eric@5stops.com>
2010-07-26 13:15:44 -0700

    Return the response back with the Transaction if the FreemiumTransaction object takes a response=.

--- lib/freemium/gateways/brain_tree.rb
+++ lib/freemium/gateways/brain_tree.rb
@@ -62,7 +62,9 @@ module Freemium
           :amount => sprintf("%.2f", amount.cents.to_f / 100)
         })
         p.commit(open_timeout, read_timeout)
-        return FreemiumTransaction.new(:billing_key => vault_id, :amount => amount, :success => p.response.success?)
+        transaction = FreemiumTransaction.new(:billing_key => vault_id, :amount => amount, :success => p.response.success?)
+        transaction.response = p.response if transaction.respond_to?(:response=)
+        return transaction
       end
 
       # Removes a card from SecureVault. Called automatically when the subscription expires.
